acf8c2cd7c2cb2f018ee39bae75b08154db596dc
Gift <giftmbanda@gmail.com>
2020-09-04 00:24:27 +0200

    added timestamp on userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,27 +1,35 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-
-  // _id: mongoose.Schema.Types.ObjectId,
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    // _id: mongoose.Schema.Types.ObjectId,
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phonenumber: {
+      type: String,
+      match: /^[0-9]{10}$/,
+    },
+    createdAt: {
+      type: Date,
+      required: false,
+    },
+    updatedAt: {
+      type: Date,
+      required: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  phonenumber: {
-    type: String,
-    match: /^[0-9]{10}$/,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-},
-{ timestamps: true });
+  { timestamps: true } //to include createdAt and updatedAt
+);
 module.exports = mongoose.model("User", userSchema);

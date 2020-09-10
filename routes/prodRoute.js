50d2915eec2ecfd519282ec2e3845cf296555e82
Gift <giftmbanda@gmail.com>
2020-09-10 20:39:48 +0200

    fixing swagger

--- routes/prodRoute.js
+++ routes/prodRoute.js
@@ -14,12 +14,12 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.imagetype === "image/png") {
-    cb(null, true);
-  } else {
-    cb("Not a valid file format", false);
-  }
-};
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
 
 const upload = multer({
   storage: storage,

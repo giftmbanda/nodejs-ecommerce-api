9cae1cacf3d167137c18c6912dd31244afeb18ce
Gift <giftmbanda@gmail.com>
2020-09-26 00:33:29 +0200

    make it better

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.use(express.static(path.join(__dirname, "uploads")));
+app.use(express.static(path.join(__dirname, "public"))); //serves resources from public folder
 // Routes which should handle requests
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument)); //still working on it
 app.use("/product", productRoute);

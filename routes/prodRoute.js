9de80aa37f083872a00d0194358891a3254147e9
Gift <giftmbanda@gmail.com>
2020-09-26 00:50:17 +0200

    make it better

--- routes/prodRoute.js
+++ routes/prodRoute.js
@@ -6,7 +6,7 @@ const ProdController = require("../controllers/prodController");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads");
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);

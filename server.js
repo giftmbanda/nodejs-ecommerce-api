3e943f0e943e5bd610e287ac8b516d663c7e9ab1
Gift <giftmbanda@gmail.com>
2020-09-25 23:56:14 +0200

    make it better

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const http = require("http");
 const app = require("./app");
-const path = require("path");
-const express = require("express");
-const app = express();
 const server = http.createServer(app);
-const publicPath = path.join(__dirname, "uploads");
-const port = process.env.PORT || 3000;
 require("./database/db"); //import the database
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
-app.use(express.static(publicPath));
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+
+// const http = require("http");
+// const app = require("./app");
+// const path = require("path");
+// const express = require("express");
+// const app = express();
+// const server = http.createServer(app);
+// const publicPath = path.join(__dirname, "uploads");
+// const port = process.env.PORT || 3000;
+// require("./database/db"); //import the database
+
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
+
+// app.use(express.static(publicPath));
+
+// server.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });

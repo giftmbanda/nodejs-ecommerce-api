d4201e68d768690b18bf40c76acb643e1014015e
Gift <giftmbanda@gmail.com>
2020-09-26 01:11:09 +0200

    make it better

--- routes/prodRoute.js
+++ routes/prodRoute.js
@@ -6,20 +6,20 @@ const ProdController = require("../controllers/prodController");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads");
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    cb(null, Date.now()+"_"+file.originalname);
   },
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.imagetype === "image/png") {
-    cb(null, true);
-  } else {
-    cb("Not a valid file format", false);
-  }
-};
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
 
 const upload = multer({
   storage: storage,

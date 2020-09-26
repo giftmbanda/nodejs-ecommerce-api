45c1000826c984bcba43ebe8735cd96165d35ebf
Gift <giftmbanda@gmail.com>
2020-09-26 16:03:13 +0200

    fixing multer

--- routes/prodRoute.js
+++ routes/prodRoute.js
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
     cb(null, true);
   } else {
     cb(null, false);

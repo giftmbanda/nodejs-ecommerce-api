4bbd97296b3213eded5d826dccd7b9e32f390546
Gift Banda <giftmbanda@gmail.com>
2020-11-01 20:48:21 +0200

    fixing orders

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const database = mongoose.connect(
-  process.env.DB_LOCAL,
+  process.env.DB_URL,
   { useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useFindAndModify: false,
